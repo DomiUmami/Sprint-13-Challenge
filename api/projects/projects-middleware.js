332fe1851d489f11bc9e0f356206bd6683ef0a1f
DomiUmami <williams_dominik@yahoo.com>
2024-05-20 08:51:27 -0400

    wip

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -2,9 +2,21 @@
 const Projects = require('../projects/projects-model')
 
 
-function validateProjectId(req, res, next){
-    console.log('validateProjectId middleware')
-    next()
+async function validateProjectId(req, res, next){
+    try {
+        const projects = await Projects.getById(req.params.id)
+        if(!projects){
+            next({ status: 404, message: 'project not found'})
+        } else {
+            req.projects = projects
+            next()
+        }
+    }
+    catch (err){
+        res.status(500).json({
+            message: 'problem finding project',
+        })
+    }
 }
 
 function validateProject(req, res, next){
